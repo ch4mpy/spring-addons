064b81f6de8e06ccd0222e16286635c7ae0abd42
ch4mpy <ch4mp@c4-soft.com>
2021-10-14 12:33:54 -1000

    @Given security-context

--- spring-security-oauth2-test-webmvc-addons/src/test/java/com/c4_soft/springaddons/samples/webmvc/keycloak/cucumber/steps/GreetingControllerSuite.java
+++ spring-security-oauth2-test-webmvc-addons/src/test/java/com/c4_soft/springaddons/samples/webmvc/keycloak/cucumber/steps/GreetingControllerSuite.java
@@ -1,13 +1,20 @@
 package com.c4_soft.springaddons.samples.webmvc.keycloak.cucumber.steps;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.context.TestSecurityContextHolder;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import com.c4_soft.springaddons.security.oauth2.test.mockmvc.keycloak.ServletKeycloakAuthUnitTestingSupport;
 
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
@@ -19,22 +26,37 @@ public class GreetingControllerSuite {
 	@Autowired
 	ServletKeycloakAuthUnitTestingSupport keycloak;
 
-	@When("unauthenticated users want to get greeting")
-	public void unauthenticatedUsersWantToGetGreeting() throws Exception {
+	MvcResult result;
+
+	@Given("user is not authenticated")
+	public void unauthenticatedUser() {
+		TestSecurityContextHolder.clearContext();
 	}
 
-	@Then("it is redirected to login")
-	public void itIsRedirectedToLogin() throws Exception {
-		mockMvc.perform(get("/greet")).andExpect(status().is3xxRedirection());
+	@Given("the following user roles:")
+	public void authenticateAsUser(List<String> rolesTable) {
+		TestSecurityContextHolder.clearContext();
+		final Stream<String> roles = rolesTable.stream().map(String::trim);
+		TestSecurityContextHolder.setAuthentication(keycloak.authentication().authorities(roles).build());
 	}
 
-	@When("authenticated users want to get greeting")
-	public void authenticatedUsersWantToGetGreeting() throws Exception {
+	@When("a get request is sent to greeting endpoint")
+	public void getGreet() throws Exception {
+		result = mockMvc.perform(get("/greet")).andReturn();
+	}
+
+	@Then("user is redirected to login")
+	public void redirectedToLogin() throws Exception {
+		assertEquals(302, result.getResponse().getStatus());
 	}
 
 	@Then("a greeting is returned")
-	public void aGreetingIsReturned() throws Exception {
-		mockMvc.perform(get("/greet").with(keycloak.authentication())).andExpect(status().isOk());
+	public void greetingIsReturned() throws Exception {
+		assertEquals(200, result.getResponse().getStatus());
+		final var body = result.getResponse().getContentAsString();
+		assertThat(body.contains("Hello user! You are granted with "));
+		assertThat(body.contains("ROLE_user"));
+		assertThat(body.contains("ROLE_TESTER"));
 	}
 
 }
--- spring-security-oauth2-test-webmvc-addons/src/test/resources/cucumber-features/greetings.feature
+++ spring-security-oauth2-test-webmvc-addons/src/test/resources/cucumber-features/greetings.feature
@@ -1,10 +1,14 @@
 Feature: Testing a secured REST API with @WithMockSomething
   Authenticated users should be able to submit GET greetings
 
-  Scenario: Unauthorized users shouldn't be greeted
-    When unauthenticated users want to get greeting
-    Then it is redirected to login
-
   Scenario: Authorized users should be greeted
-    When authenticated users want to get greeting
+    Given the following user roles:
+      | ROLE_user   |
+      | ROLE_TESTER |
+    When a get request is sent to greeting endpoint
     Then a greeting is returned
+
+  Scenario: Unauthorized users shouldn't be greeted
+    Given user is not authenticated
+    When a get request is sent to greeting endpoint
+    Then user is redirected to login
