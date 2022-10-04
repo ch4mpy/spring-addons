a8848e2e217c8004e7dee3a45279c724eb1a4b48
ch4mpy <ch4mp@c4-soft.com>
2022-10-04 12:11:43 -1000

    gh-64 Include jwt or introspection in sample names

---
+++ samples/webflux-jwt-default/src/test/java/com/c4_soft/springaddons/samples/webflux_jwtauthenticationtoken/GreetingControllerAnnotatedTest.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2019 Jérôme Wacongne.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package com.c4_soft.springaddons.samples.webflux_jwtauthenticationtoken;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+
+import com.c4_soft.springaddons.security.oauth2.test.annotations.OpenIdClaims;
+import com.c4_soft.springaddons.security.oauth2.test.annotations.WithMockAuthentication;
+import com.c4_soft.springaddons.security.oauth2.test.annotations.WithMockJwtAuth;
+import com.c4_soft.springaddons.security.oauth2.test.webflux.WebTestClientSupport;
+import com.c4_soft.springaddons.security.oauth2.test.webflux.jwt.AutoConfigureAddonsWebSecurity;
+
+import reactor.core.publisher.Mono;
+
+/**
+ * <h2>Unit-test a secured controller</h2>
+ *
+ * @author Jérôme Wacongne &lt;ch4mp&#64;c4-soft.com&gt;
+ */
+
+@WebFluxTest(GreetingController.class) // Use WebFluxTest or WebMvcTest
+@AutoConfigureAddonsWebSecurity // If your web-security depends on it, setup spring-addons security
+@Import({ WebfluxJwtDefault.SecurityConfig.class }) // Import your web-security configuration
+class GreetingControllerAnnotatedTest {
+
+	// Mock controller injected dependencies
+	@MockBean
+	private MessageService messageService;
+
+	@Autowired
+	WebTestClientSupport api;
+
+	@BeforeEach
+	public void setUp() {
+		when(messageService.greet(any())).thenAnswer(invocation -> {
+			final JwtAuthenticationToken auth = invocation.getArgument(0, JwtAuthenticationToken.class);
+			return Mono.just(String.format("Hello %s! You are granted with %s.", auth.getName(), auth.getAuthorities()));
+		});
+		when(messageService.getSecret()).thenReturn(Mono.just("Secret message"));
+	}
+
+	@Test
+	void greetWitoutAuthentication() throws Exception {
+		api.get("https://localhost/greet").expectStatus().isUnauthorized();
+	}
+
+	@Test
+	@WithMockAuthentication(authType = JwtAuthenticationToken.class, authorities = "ROLE_AUTHORIZED_PERSONNEL")
+	void greetWithDefaultMockAuthentication() throws Exception {
+		api.get("https://localhost/greet").expectBody(String.class).isEqualTo("Hello user! You are granted with [ROLE_AUTHORIZED_PERSONNEL].");
+	}
+
+	@Test
+	@WithMockJwtAuth()
+	void greetWithDefaultJwtAuthentication() throws Exception {
+		api.get("https://localhost/greet").expectBody(String.class).isEqualTo("Hello user! You are granted with [ROLE_USER].");
+	}
+
+	@Test
+	@WithMockAuthentication(authType = JwtAuthenticationToken.class, name = "Ch4mpy", authorities = "ROLE_AUTHORIZED_PERSONNEL")
+	void greetMockCh4mpy() throws Exception {
+		api.get("https://localhost/greet").expectBody(String.class).isEqualTo("Hello Ch4mpy! You are granted with [ROLE_AUTHORIZED_PERSONNEL].");
+	}
+
+	@Test
+	@WithMockJwtAuth(authorities = "ROLE_AUTHORIZED_PERSONNEL", claims = @OpenIdClaims(sub = "Ch4mpy"))
+	void greetJwtCh4mpy() throws Exception {
+		api.get("https://localhost/greet").expectBody(String.class).isEqualTo("Hello Ch4mpy! You are granted with [ROLE_AUTHORIZED_PERSONNEL].");
+	}
+
+	@Test
+	@WithMockAuthentication(authType = JwtAuthenticationToken.class)
+	void securedRouteWithoutAuthorizedPersonnelIsForbidden() throws Exception {
+		api.get("https://localhost/secured-route").expectStatus().isForbidden();
+	}
+
+	@Test
+	@WithMockAuthentication(authType = JwtAuthenticationToken.class, authorities = "ROLE_AUTHORIZED_PERSONNEL")
+	void securedRouteWithAuthorizedPersonnelIsOk() throws Exception {
+		api.get("https://localhost/secured-route").expectStatus().isOk();
+	}
+
+	@Test
+	@WithMockAuthentication(authType = JwtAuthenticationToken.class)
+	void securedMethodWithoutAuthorizedPersonnelIsForbidden() throws Exception {
+		api.get("https://localhost/secured-method").expectStatus().isForbidden();
+	}
+
+	@Test
+	@WithMockAuthentication(authType = JwtAuthenticationToken.class, authorities = "ROLE_AUTHORIZED_PERSONNEL")
+	void securedMethodWithAuthorizedPersonnelIsOk() throws Exception {
+		api.get("https://localhost/secured-method").expectStatus().isOk();
+	}
+}
---
+++ samples/webflux-jwt-oauthentication/src/test/java/com/c4_soft/springaddons/samples/webflux_oidcauthentication/SecretRepoTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2019 Jérôme Wacongne.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package com.c4_soft.springaddons.samples.webflux_oidcauthentication;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.c4_soft.springaddons.security.oauth2.test.annotations.OpenId;
+import com.c4_soft.springaddons.security.oauth2.test.annotations.OpenIdClaims;
+import com.c4_soft.springaddons.security.oauth2.test.webflux.jwt.AutoConfigureAddonsSecurity;
+
+/**
+ * <h2>Unit-test a secured service or repository which has no dependencies</h2>
+ *
+ * @author Jérôme Wacongne &lt;ch4mp&#64;c4-soft.com&gt;
+ */
+
+// Import security configuration and test component
+@Import({ ServerProperties.class, OAuth2ResourceServerProperties.class, WebfluxJwtOauthentication.SecurityConfig.class, SecretRepo.class })
+@AutoConfigureAddonsSecurity
+@ExtendWith(SpringExtension.class)
+class SecretRepoTest {
+
+	// auto-wire tested component
+	@Autowired
+	SecretRepo secretRepo;
+
+	@Test
+	void whenNotAuthenticatedThenThrows() {
+		// call tested components methods directly (do not use MockMvc nor WebTestClient)
+		assertThrows(Exception.class, () -> secretRepo.findSecretByUsername("ch4mpy").block());
+	}
+
+	@Test
+	@OpenId(claims = @OpenIdClaims(preferredUsername = "Tonton Pirate"))
+	void whenAuthenticatedAsSomeoneElseThenThrows() {
+		assertThrows(Exception.class, () -> secretRepo.findSecretByUsername("ch4mpy").block());
+	}
+
+	@Test
+	@OpenId(claims = @OpenIdClaims(preferredUsername = "ch4mpy"))
+	void whenAuthenticatedWithSameUsernameThenReturns() {
+		assertEquals("Don't ever tell it", secretRepo.findSecretByUsername("ch4mpy").block());
+	}
+
+}
