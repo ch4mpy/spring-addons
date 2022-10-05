5e7a3d3f280dc7f331ed886b25bc8e3514767d3e
ch4mpy <ch4mp@c4-soft.com>
2022-10-05 07:59:46 -1000

    gh-59
    Sample for reactive (webflux) resource-server with introspection and BearerTokenAuthentication

---
+++ samples/webflux-introspecting-default/src/main/java/com/c4_soft/springaddons/samples/webflux_jwtauthenticationtoken/WebfluxIntrospectingDefault.java
@@ -0,0 +1,31 @@
+package com.c4_soft.springaddons.samples.webflux_jwtauthenticationtoken;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+
+import com.c4_soft.springaddons.security.oauth2.config.reactive.AuthorizeExchangeSpecPostProcessor;
+
+@SpringBootApplication
+public class WebfluxIntrospectingDefault {
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(WebfluxIntrospectingDefault.class).web(WebApplicationType.REACTIVE).run(args);
+	}
+
+	@EnableReactiveMethodSecurity()
+	public static class SecurityConfig {
+
+		@Bean
+		public AuthorizeExchangeSpecPostProcessor authorizeExchangeSpecPostProcessor() {
+			// @formatter:off
+			return (ServerHttpSecurity.AuthorizeExchangeSpec spec) -> spec
+					.pathMatchers("/secured-route").hasRole("AUTHORIZED_PERSONNEL")
+					.anyExchange().authenticated();
+			// @formatter:on
+		}
+
+	}
+}
