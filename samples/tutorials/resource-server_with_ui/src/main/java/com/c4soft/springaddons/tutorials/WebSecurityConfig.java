2e09ea3f0ef2f65b0f7dc5c49cb975b43d2f5f4f
ch4mpy <ch4mp@c4-soft.com>
2023-02-13 18:14:37 -1000

    External Authorities mapper in client

--- samples/tutorials/resource-server_with_ui/src/main/java/com/c4soft/springaddons/tutorials/WebSecurityConfig.java
+++ samples/tutorials/resource-server_with_ui/src/main/java/com/c4soft/springaddons/tutorials/WebSecurityConfig.java
@@ -1,7 +1,9 @@
 package com.c4soft.springaddons.tutorials;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.context.annotation.Bean;
@@ -14,11 +16,14 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.OrRequestMatcher;
 
+import com.c4_soft.springaddons.security.oauth2.config.ConfigurableClaimSet2AuthoritiesConverter;
 import com.c4_soft.springaddons.security.oauth2.config.synchronised.ExpressionInterceptUrlRegistryPostProcessor;
 
 @Configuration
@@ -27,7 +32,8 @@ public class WebSecurityConfig {
 
 	/**
 	 * <p>
-	 * A default SecurityFilterChain is already defined by spring-addons-webmvc-jwt-resource-server to secure all API endpoints (actuator and REST controllers)
+	 * A default SecurityFilterChain is already defined by spring-addons-webmvc-jwt-resource-server to secure all API endpoints (actuator and
+	 * REST controllers)
 	 * </p>
 	 * We define here another SecurityFilterChain for server-side rendered pages:
 	 * <ul>
@@ -36,9 +42,9 @@ public class WebSecurityConfig {
 	 * <li>Thymeleaf pages served by UiController</li>
 	 * </ul>
 	 * <p>
-	 * It important to note that in this scenario, the end-user browser is not an OAuth2 client. Only the part of the server-side part of the Spring application
-	 * secured with this filter chain is. Requests between the browser and Spring OAuth2 client are secured with <b>sessions</b>. As so, <b>CSRF protection must
-	 * be active</b>.
+	 * It important to note that in this scenario, the end-user browser is not an OAuth2 client. Only the part of the server-side part of the
+	 * Spring application secured with this filter chain is. Requests between the browser and Spring OAuth2 client are secured with
+	 * <b>sessions</b>. As so, <b>CSRF protection must be active</b>.
 	 * </p>
 	 *
 	 * @param  http
@@ -48,11 +54,7 @@ public class WebSecurityConfig {
 	 */
 	@Order(Ordered.HIGHEST_PRECEDENCE)
 	@Bean
-	SecurityFilterChain uiFilterChain(
-			HttpSecurity http,
-			ServerProperties serverProperties,
-			Converter<Map<String, Object>, Collection<? extends GrantedAuthority>> authoritiesConverter)
-			throws Exception {
+	SecurityFilterChain uiFilterChain(HttpSecurity http, ServerProperties serverProperties, GrantedAuthoritiesMapper authoritiesMapper) throws Exception {
 		boolean isSsl = serverProperties.getSsl() != null && serverProperties.getSsl().isEnabled();
 
 		// @formatter:off
@@ -71,14 +73,10 @@ public class WebSecurityConfig {
 			    .loginPage("%s://localhost:%d/oauth2/authorization/spring-addons-public".formatted(isSsl ? "https" : "http", serverProperties.getPort()) )
 				// I don't know quite why we are redirected to authorization-server port by default as we initially tried to access a client resource :/
 			    .defaultSuccessUrl("%s://localhost:%d/ui/index.html".formatted(isSsl ? "https" : "http", serverProperties.getPort()), true)
-				// This is how to map authorities from ID-token claims of our choice (instead of just `scp` claim)
+				// This is how to map authorities from ID claims issued by an OIDC provider of our choice (instead of just `scp` claim)
 				// Here we make use of the authorities mapper already defined by spring-addons for default filter-chain (the one for resource-server)
 				// Refer to your authorization doc if it does not include roles to ID-tokens by default. For Keycloak, "realm roles" & "client roles" mappers must be added in clients -> {your client} -> Client scopes -> {your client}-dedicated -> Add mapper.
-			    .userInfoEndpoint().userAuthoritiesMapper((authorities) -> authorities.stream()
-	        		.filter(a -> OidcUserAuthority.class.isAssignableFrom(a.getClass()))
-	        		.map(OidcUserAuthority.class::cast)
-	        		.flatMap(oua -> authoritiesConverter.convert(oua.getIdToken().getClaims()).stream()).toList()
-	        );
+			    .userInfoEndpoint().userAuthoritiesMapper(authoritiesMapper);
 
 	    http.authorizeHttpRequests()
 	            .requestMatchers("/login/**").permitAll()
@@ -109,5 +107,28 @@ public class WebSecurityConfig {
                 .requestMatchers("/actuator/**").hasAuthority("OBSERVABILITY:write")
                 .anyRequest().authenticated();
         // @formatter:on
+	}
+
+	/**
+	 * @param  authoritiesConverter We are in spring-addons, we have a {@link ConfigurableClaimSet2AuthoritiesConverter} in the context!
+	 * @return                      a mapper from oauth2Login result to granted authorities
+	 */
+	@Bean
+	GrantedAuthoritiesMapper userAuthoritiesMapper(Converter<Map<String, Object>, Collection<? extends GrantedAuthority>> authoritiesConverter) {
+		return (authorities) -> {
+			Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
+
+			authorities.forEach(authority -> {
+				if (authority instanceof OidcUserAuthority oidcAuth) {
+					mappedAuthorities.addAll(authoritiesConverter.convert(oidcAuth.getIdToken().getClaims()));
+
+				} else if (authority instanceof OAuth2UserAuthority oauth2Auth) {
+					mappedAuthorities.addAll(authoritiesConverter.convert(oauth2Auth.getAttributes()));
+
+				}
+			});
+
+			return mappedAuthorities;
+		};
 	}
 }
