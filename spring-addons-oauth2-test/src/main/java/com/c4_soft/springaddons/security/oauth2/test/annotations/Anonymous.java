aa2015932d7e7991543d8d376dfb62ca23dc1ee0
ch4mpy <ch4mp@c4-soft.com>
2023-02-16 11:45:51 -1000

    Add @WithAnonymous

---
+++ spring-addons-oauth2-test/src/main/java/com/c4_soft/springaddons/security/oauth2/test/annotations/Anonymous.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2019 Jérôme Wacongne.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package com.c4_soft.springaddons.security.oauth2.test.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.TestExecutionEvent;
+import org.springframework.security.test.context.support.WithSecurityContext;
+import org.springframework.security.test.context.support.WithSecurityContextFactory;
+import org.springframework.test.context.TestContext;
+
+/**
+ * <p>
+ * Populates {@link SecurityContext} with an
+ * {@link AnonymousAuthenticationToken}.
+ * </p>
+ * Sample usage:
+ *
+ * <pre>
+ * &#64;Test
+ * &#64;WithMockAuthentication
+ * public demoDefaultUserNameAndAuthorities {
+ *   // test as "user" granted without any authority
+ * }
+ *
+ * &#64;Test
+ * &#64;WithMockAuthentication(name = "Ch4mpy", authorities = { "ROLE_TESTER", "ROLE_AUTHOR" })
+ * public demoCustomUserNameAndAuthorities {
+ *   // test as "Ch4mpy" granted with "ROLE_TESTER", "ROLE_AUTHOR"
+ * }
+ *
+ * &#64;Test
+ * &#64;WithMockAuthentication(JwtAuthenticationToken.class)
+ * public demoCustomAuthenticationImpl {
+ *   final var jwt = mock(Jwt.class);
+ *   when(jwt.getSubject()).thenReturn(auth.getName());
+ *
+ *   final var auth = (JwtAuthenticationToken) SecurityContextHolder.getContext();
+ *   when(auth.getPrincipal()).thenReturn(jwt);
+ *
+ *   // test as "user" granted without any authority, the Authentication in the SecurityContext being a JwtAuthenticationToken mock
+ * }
+ * </pre>
+ *
+ * @author Jérôme Wacongne &lt;ch4mp&#64;c4-soft.com&gt;
+ */
+@Target({ ElementType.METHOD, ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+@WithSecurityContext(factory = Anonymous.Factory.class)
+public @interface Anonymous {
+
+    /**
+     * Determines when the {@link SecurityContext} is setup. The default is before
+     * {@link TestExecutionEvent#TEST_METHOD} which occurs during
+     * {@link org.springframework.test.context.TestExecutionListener#beforeTestMethod(TestContext)}
+     *
+     * @return the {@link TestExecutionEvent} to initialize before
+     */
+    @AliasFor(annotation = WithSecurityContext.class)
+    TestExecutionEvent setupBefore() default TestExecutionEvent.TEST_METHOD;
+
+    public static final class Factory implements WithSecurityContextFactory<Anonymous> {
+        private static final AnonymousAuthenticationToken ANONYMOUS_AUTHENTICATION = new AnonymousAuthenticationToken(
+                "anonymous",
+                "anonymousUser",
+                AuthorityUtils.createAuthorityList("ROLE_ANONYMOUS"));
+
+        @Override
+        public SecurityContext createSecurityContext(Anonymous annotation) {
+            final SecurityContext context = SecurityContextHolder.createEmptyContext();
+            context.setAuthentication(authentication(annotation));
+
+            return context;
+        }
+
+        public AnonymousAuthenticationToken authentication(Anonymous annotation) {
+            return ANONYMOUS_AUTHENTICATION;
+        }
+    }
+}
--- webmvc/spring-addons-webmvc-core/src/main/java/com/c4_soft/springaddons/security/oauth2/config/synchronised/OAuth2AuthenticationFactory.java
+++ webmvc/spring-addons-webmvc-core/src/main/java/com/c4_soft/springaddons/security/oauth2/config/synchronised/OAuth2AuthenticationFactory.java
@@ -4,7 +4,6 @@ import java.util.Map;
 
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 
-// FIXME: replace with the outcome of when https://github.com/spring-projects/spring-security/issues/11661
 public interface OAuth2AuthenticationFactory {
-	AbstractAuthenticationToken build(String bearerString, Map<String, Object> claims);
+    AbstractAuthenticationToken build(String bearerString, Map<String, Object> claims);
 }
