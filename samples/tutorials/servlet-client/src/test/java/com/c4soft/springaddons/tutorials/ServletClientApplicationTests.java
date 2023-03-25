1bf7288b542bba1095eb2df7c0623d9fac74c5e2
ch4mpy <ch4mp@c4-soft.com>
2023-03-24 14:28:04 -1000

    Cleanup tutorials dependency management

--- samples/tutorials/servlet-client/src/test/java/com/c4soft/springaddons/tutorials/ServletClientApplicationTests.java
+++ samples/tutorials/servlet-client/src/test/java/com/c4soft/springaddons/tutorials/ServletClientApplicationTests.java
@@ -1,13 +1,37 @@
 package com.c4soft.springaddons.tutorials;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+@Import(ServletClientApplicationTests.TestSecurityConf.class)
 class ServletClientApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
+	@TestConfiguration
+	static class TestSecurityConf {
+		@Bean
+		InMemoryClientRegistrationRepository clientRegistrationRepository() {
+			final var clientRegistrationRepository = mock(InMemoryClientRegistrationRepository.class);
+			when(clientRegistrationRepository.iterator()).thenReturn(new ArrayList<ClientRegistration>().iterator());
+			when(clientRegistrationRepository.spliterator()).thenReturn(new ArrayList<ClientRegistration>().spliterator());
+			return clientRegistrationRepository;
+		}
+	}
 }
