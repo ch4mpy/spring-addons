d01923d00757fe3575dbc5afd20313f2d9245957
ch4mpy <ch4mp@c4-soft.com>
2023-12-22 07:34:33 -1000

    Boot 3.2.1

--- samples/tutorials/resource-server_with_ui/src/main/java/com/c4soft/springaddons/tutorials/ui/ClientBearerRequestInterceptor.java
+++ samples/tutorials/resource-server_with_ui/src/main/java/com/c4soft/springaddons/tutorials/ui/ClientBearerRequestInterceptor.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -15,19 +16,22 @@ import lombok.RequiredArgsConstructor;
 @Component
 @RequiredArgsConstructor
 public class ClientBearerRequestInterceptor implements RequestInterceptor {
-	private final OAuth2AuthorizedClientRepository authorizedClientRepo;
+    private final OAuth2AuthorizedClientRepository authorizedClientRepo;
 
-	@Override
-	public void apply(RequestTemplate template) {
-		final var auth = SecurityContextHolder.getContext().getAuthentication();
-		if (RequestContextHolder.getRequestAttributes() instanceof ServletRequestAttributes servletRequestAttributes) {
-			if (auth instanceof OAuth2AuthenticationToken oauth) {
-				final var authorizedClient =
-						authorizedClientRepo.loadAuthorizedClient(oauth.getAuthorizedClientRegistrationId(), auth, servletRequestAttributes.getRequest());
-				if (authorizedClient != null) {
-					template.header(HttpHeaders.AUTHORIZATION, "Bearer %s".formatted(authorizedClient.getAccessToken().getTokenValue()));
-				}
-			}
-		}
-	}
+    @Override
+    public void apply(RequestTemplate template) {
+        final var auth = SecurityContextHolder.getContext().getAuthentication();
+        if (RequestContextHolder.getRequestAttributes() instanceof ServletRequestAttributes servletRequestAttributes) {
+            if (auth instanceof OAuth2AuthenticationToken oauth) {
+                final var authorizedClient = authorizedClientRepo
+                    .loadAuthorizedClient(oauth.getAuthorizedClientRegistrationId(), auth, servletRequestAttributes.getRequest());
+                if (authorizedClient != null) {
+                    template.header(HttpHeaders.AUTHORIZATION, "Bearer %s".formatted(authorizedClient.getAccessToken().getTokenValue()));
+                }
+            }
+            if (auth instanceof JwtAuthenticationToken jwtAuth) {
+                template.header(HttpHeaders.AUTHORIZATION, "Bearer %s".formatted(jwtAuth.getToken().getTokenValue()));
+            }
+        }
+    }
 }
