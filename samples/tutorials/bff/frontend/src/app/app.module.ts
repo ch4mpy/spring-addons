c5406228bf1655c3e00b7cc1bfdfa7a0b62c6666
ph <ph@phw.devfra>
2024-01-01 13:25:32 +0100

    Remind the user to omit the protocol prefix for POST, DELETE and PUT requests, else Angular will not provide an XSRF header. Example: `gatewayUri = '//localhost:8080'`.  See https://github.com/angular/angular/issues/20511

--- samples/tutorials/bff/frontend/src/app/app.module.ts
+++ samples/tutorials/bff/frontend/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 export const gatewayUri = 'https://localhost:8080';
+// For Angular's HttpClient omit the protocol prefix for POST, DELETE and PUT requests, else Angular will not provide
+// an XSRF header. Example: gatewayUri = '//localhost:8080'. See https://github.com/angular/angular/issues/20511
 export const apiUri = `${gatewayUri}/bff/v1`;
 export const greetingApiUri = `${apiUri}/greeting`;
 export const usersApiUri = `${apiUri}/users`;
