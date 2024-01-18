7d2fa93eb2fcc35b3f83697691fcd44249941a47
ch4mpy <ch4mp@c4-soft.com>
2024-01-17 16:15:56 -1000

    Fix post login/logout headers & params detection in reactive apps

--- spring-addons-starter-oidc/src/main/java/com/c4_soft/springaddons/security/oidc/starter/properties/condition/bean/DefaultAuthenticationFailureHandlerCondition.java
+++ spring-addons-starter-oidc/src/main/java/com/c4_soft/springaddons/security/oidc/starter/properties/condition/bean/DefaultAuthenticationFailureHandlerCondition.java
@@ -1,50 +1,22 @@
 package com.c4_soft.springaddons.security.oidc.starter.properties.condition.bean;
 
-import org.springframework.boot.autoconfigure.condition.AllNestedConditions;
-import org.springframework.boot.autoconfigure.condition.AnyNestedCondition;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.NoneNestedConditions;
-import org.springframework.context.annotation.Conditional;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.server.authentication.ServerAuthenticationFailureHandler;
 
-public class DefaultAuthenticationFailureHandlerCondition extends AllNestedConditions {
+public class DefaultAuthenticationFailureHandlerCondition extends NoneNestedConditions {
 
-    public DefaultAuthenticationFailureHandlerCondition() {
-        super(ConfigurationPhase.REGISTER_BEAN);
-    }
+	public DefaultAuthenticationFailureHandlerCondition() {
+		super(ConfigurationPhase.REGISTER_BEAN);
+	}
 
-    @Conditional(NoAuthenticationFailureHandlerCondition.class)
-    static class AuthenticationFailureHandlerMissingCondition {}
+	@ConditionalOnBean(AuthenticationFailureHandler.class)
+	static class AuthenticationFailureHandlerProvidedCondition {
+	}
 
-    @Conditional(PostLoginRedirectUriCondition.class)
-    static class PostLoginRedirectUriProvidedCondition {}
-
-    static class NoAuthenticationFailureHandlerCondition extends NoneNestedConditions {
-
-        public NoAuthenticationFailureHandlerCondition() {
-            super(ConfigurationPhase.REGISTER_BEAN);
-        }
-
-        @ConditionalOnBean(AuthenticationFailureHandler.class)
-        static class AuthenticationFailureHandlerProvidedCondition {}
-
-        @ConditionalOnBean(ServerAuthenticationFailureHandler.class)
-        static class ServerAuthenticationFailureHandlerProvidedCondition {}
-    }
-
-    static class PostLoginRedirectUriCondition extends AnyNestedCondition {
-
-        public PostLoginRedirectUriCondition() {
-            super(ConfigurationPhase.REGISTER_BEAN);
-        }
-
-        @ConditionalOnProperty(name = "com.c4-soft.springaddons.oidc.client.post-login-redirect-host")
-        static class PostLoginRedirectHostCondition {}
-
-        @ConditionalOnProperty(name = "com.c4-soft.springaddons.oidc.client.post-login-redirect-path")
-        static class PostLoginRedirectPathCondition {}
-    }
+	@ConditionalOnBean(ServerAuthenticationFailureHandler.class)
+	static class ServerAuthenticationFailureHandlerProvidedCondition {
+	}
 
 }
