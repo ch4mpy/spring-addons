7a203a78df730c1b3673c5e353c2b9e9d2f85f77
ch4mpy <ch4mp@c4-soft.com>
2024-05-15 12:55:28 -1000

    Javadoc for @WithOAuth2Login and @WithOidcLogin

--- spring-addons-oauth2-test/src/main/java/com/c4_soft/springaddons/security/oauth2/test/annotations/WithOAuth2Login.java
+++ spring-addons-oauth2-test/src/main/java/com/c4_soft/springaddons/security/oauth2/test/annotations/WithOAuth2Login.java
@@ -8,12 +8,43 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import org.springframework.core.annotation.AliasFor;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.jwt.JwtClaimNames;
 import org.springframework.security.test.context.support.TestExecutionEvent;
 import org.springframework.security.test.context.support.WithSecurityContext;
 
+/**
+ * <p>
+ * Populates the test security context with an {@link OAuth2AuthenticationToken} instance with a {@link DefaultOAuth2User} as principal.
+ * </p>
+ * <p>
+ * Only the annotation properties are used to build the authentication Instance. <b>Neither {@link OAuth2UserService} nor
+ * {@link GrantedAuthoritiesMapper} are called.</b>
+ * </p>
+ * Usage to define just authorities:
+ * 
+ * <pre>
+ * &#64;WithOAuth2Login({"BIDULE", "CHOSE"})
+ * </pre>
+ * 
+ * Advanced usage to set any claims, including private ones:
+ * 
+ * <pre>
+ * &#64;WithOAuth2Login(
+ *   authorities = {"NICE"},
+ *   nameAttributeKey = StandardClaimNames.PREFERRED_USERNAME,
+ *   claims = &#64;OpenIdClaims(
+ *     preferredUsername = "tonton-pirate",
+ *     email = "tonton@c4-soft.com",
+ *     otherClaims =  &#64;Claims(
+ *       stringClaims = { &#64;StringClaim(name = "machin", value = "truc") })))
+ * </pre>
+ * 
+ * @author ch4mp&#64;c4-soft.com
+ */
 @Target({ ElementType.METHOD, ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
@@ -34,7 +65,8 @@ public @interface WithOAuth2Login {
 	String authorizedClientRegistrationId() default "bidule";
 
 	/**
-	 * @return the key used to access the user's &quot;name&quot; from claims. This takes precedence over OpenIdClaims::usernameClaim if both are defined
+	 * @return the key used to access the user's &quot;name&quot; from claims. This takes precedence over OpenIdClaims::usernameClaim if both
+	 *         are defined
 	 */
 	String nameAttributeKey() default JwtClaimNames.SUB;
 
