3df1edd70606c455578181852a80209a335ff1bc
ch4mpy <ch4mp@c4-soft.com>
2024-09-24 13:42:31 -1000

    Prepare 7.9.0:
    Ease OAuth2 clients response status for unauthorized requests (return
    401 in case of single page or mobile frontends)a.
    
    Enable Back-Channel Logout RP endpoint for Spring Clients with
    oauth2Login.

--- spring-addons-starter-oidc/src/main/java/com/c4_soft/springaddons/security/oidc/starter/synchronised/client/SpringAddonsOauth2RedirectStrategy.java
+++ spring-addons-starter-oidc/src/main/java/com/c4_soft/springaddons/security/oidc/starter/synchronised/client/SpringAddonsOauth2RedirectStrategy.java
@@ -10,25 +10,28 @@ import com.c4_soft.springaddons.security.oidc.starter.properties.SpringAddonsOid
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 /**
- * A redirect strategy that might not actually redirect: the HTTP status is taken from com.c4-soft.springaddons.oidc.client.oauth2-redirect-status property.
- * User-agents will auto redirect only if the status is in 3xx range. This gives single page and mobile applications a chance to intercept the redirection and
- * choose to follow the redirection (or not), with which agent and potentially by clearing some headers.
+ * A redirect strategy that might not actually redirect: the HTTP status is taken from
+ * com.c4-soft.springaddons.oidc.client.oauth2-redirect-status property. User-agents will auto redirect only if the status is in 3xx range.
+ * This gives single page and mobile applications a chance to intercept the redirection and choose to follow the redirection (or not), with
+ * which agent and potentially by clearing some headers.
  *
  * @author Jerome Wacongne ch4mp&#64;c4-soft.com
  */
 @RequiredArgsConstructor
 public class SpringAddonsOauth2RedirectStrategy implements RedirectStrategy {
 
-    private final HttpStatus defaultStatus;
+	@Getter
+	private final HttpStatus defaultStatus;
 
-    @Override
-    public void sendRedirect(HttpServletRequest request, HttpServletResponse response, String location) throws IOException {
-        final var requestedStatus = request.getIntHeader(SpringAddonsOidcClientProperties.RESPONSE_STATUS_HEADER);
-        response.setStatus(requestedStatus > -1 ? requestedStatus : defaultStatus.value());
+	@Override
+	public void sendRedirect(HttpServletRequest request, HttpServletResponse response, String location) throws IOException {
+		final var requestedStatus = request.getIntHeader(SpringAddonsOidcClientProperties.RESPONSE_STATUS_HEADER);
+		response.setStatus(requestedStatus > -1 ? requestedStatus : defaultStatus.value());
 
-        response.setHeader(HttpHeaders.LOCATION, location);
-    }
+		response.setHeader(HttpHeaders.LOCATION, location);
+	}
 }
