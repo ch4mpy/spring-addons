2526cb06c5d5a23a92143ece5fcd3377ed3dce2b
ch4mpy <ch4mp@c4-soft.com>
2024-09-27 15:45:38 -1000

    Prepare the 7.9.0-M3 release

--- samples/tutorials/resource-server_with_ui/src/main/java/com/c4soft/springaddons/tutorials/ResourceServerWithUiApplication.java
+++ samples/tutorials/resource-server_with_ui/src/main/java/com/c4soft/springaddons/tutorials/ResourceServerWithUiApplication.java
@@ -2,10 +2,8 @@ package com.c4soft.springaddons.tutorials;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 public class ResourceServerWithUiApplication {
 
 	public static void main(String[] args) {
