b6414d68f85f1eb156533baab1fd0a5d5f3b58c4
ch4mpy <ch4mp@c4-soft.com>
2024-11-04 12:27:25 -1000

    Restore Proxy-Authorization header for RestClient

--- spring-addons-starter-rest/src/main/java/com/c4_soft/springaddons/rest/SpringAddonsClientHttpRequestFactory.java
+++ spring-addons-starter-rest/src/main/java/com/c4_soft/springaddons/rest/SpringAddonsClientHttpRequestFactory.java
@@ -4,25 +4,36 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
 import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.Optional;
 import java.util.regex.Pattern;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.client.ClientHttpRequest;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
+import org.springframework.util.StringUtils;
 import com.c4_soft.springaddons.rest.SpringAddonsRestProperties.RestClientProperties.ClientHttpRequestFactoryProperties;
 
 /**
+ * <p>
  * A wrapper around {@link SimpleClientHttpRequestFactory} that sends the request through an HTTP or
- * SOCKS proxy when it is enabled and when the request URI does not match the NO_PROXY pattern
+ * SOCKS proxy when it is enabled and when the request URI does not match the NO_PROXY pattern.
+ * </p>
+ * <p>
+ * When going through a proxy, the Proxy-Authorization header is set if username and password are
+ * non-empty.
+ * </p>
  * 
  * @author Jérôme Wacongne &lt;ch4mp#64;c4-soft.com&gt;
  */
 public class SpringAddonsClientHttpRequestFactory implements ClientHttpRequestFactory {
   private final Optional<Pattern> nonProxyHostsPattern;
-  private final SimpleClientHttpRequestFactory proxyDelegate;
-  private final SimpleClientHttpRequestFactory noProxyDelegate;
+  private final ClientHttpRequestFactory proxyDelegate;
+  private final ClientHttpRequestFactory noProxyDelegate;
 
   public SpringAddonsClientHttpRequestFactory(SystemProxyProperties systemProperties,
       ClientHttpRequestFactoryProperties addonsProperties) {
@@ -35,11 +46,7 @@ public class SpringAddonsClientHttpRequestFactory implements ClientHttpRequestFa
     this.noProxyDelegate = from(addonsProperties);
 
     if (proxySupport.isEnabled()) {
-      this.proxyDelegate = from(addonsProperties);
-      final var address =
-          new InetSocketAddress(proxySupport.getHostname().get(), proxySupport.getPort());
-      final var proxy = new Proxy(protocolToProxyType(proxySupport.getProtocol()), address);
-      this.proxyDelegate.setProxy(proxy);
+      this.proxyDelegate = new ProxyAwareClientHttpRequestFactory(proxySupport, addonsProperties);
     } else {
       this.proxyDelegate = this.noProxyDelegate;
     }
@@ -79,6 +86,35 @@ public class SpringAddonsClientHttpRequestFactory implements ClientHttpRequestFa
     properties.getReadTimeoutMillis().ifPresent(requestFactory::setReadTimeout);
     properties.getChunkSize().ifPresent(requestFactory::setChunkSize);
     return requestFactory;
+  }
+
+  public static class ProxyAwareClientHttpRequestFactory implements ClientHttpRequestFactory {
+    private final SimpleClientHttpRequestFactory delegate;
+    private final @Nullable String username;
+    private final @Nullable String password;
+
+    public ProxyAwareClientHttpRequestFactory(ProxySupport proxySupport,
+        ClientHttpRequestFactoryProperties properties) {
+      this.username = proxySupport.getUsername();
+      this.password = proxySupport.getPassword();
+      this.delegate = SpringAddonsClientHttpRequestFactory.from(properties);
+      final var address =
+          new InetSocketAddress(proxySupport.getHostname().get(), proxySupport.getPort());
+      final var proxy = new Proxy(protocolToProxyType(proxySupport.getProtocol()), address);
+      this.delegate.setProxy(proxy);
+    }
+
+    @SuppressWarnings("null")
+    @Override
+    public ClientHttpRequest createRequest(URI uri, HttpMethod httpMethod) throws IOException {
+      final var request = delegate.createRequest(uri, httpMethod);
+      if (StringUtils.hasText(username) && StringUtils.hasText(password)) {
+        final var base64 = Base64.getEncoder()
+            .encodeToString((username + ':' + password).getBytes(StandardCharsets.UTF_8));
+        request.getHeaders().set(HttpHeaders.PROXY_AUTHORIZATION, "Basic %s".formatted(base64));
+      }
+      return request;
+    }
   }
 
 }
