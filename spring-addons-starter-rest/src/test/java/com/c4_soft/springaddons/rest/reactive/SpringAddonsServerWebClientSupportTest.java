22b63b47fbc4e8a089a2fc079dca138cd93d1eb4
Miguel Ángel <MiguelRG5605@Outlook.es>
2025-04-17 02:09:45 +0200

    Replace manual security context holder mocking for Spring Addons OIDC Test

--- spring-addons-starter-rest/src/test/java/com/c4_soft/springaddons/rest/reactive/SpringAddonsServerWebClientSupportTest.java
+++ spring-addons-starter-rest/src/test/java/com/c4_soft/springaddons/rest/reactive/SpringAddonsServerWebClientSupportTest.java
@@ -1,21 +1,15 @@
 package com.c4_soft.springaddons.rest.reactive;
 
+import com.c4_soft.springaddons.security.oauth2.test.annotations.WithJwt;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.matching.ContainsPattern;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-import org.springframework.security.test.context.TestSecurityContextHolder;
-import org.springframework.security.test.context.support.ReactorContextTestExecutionListener;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestExecutionListener;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.wiremock.spring.EnableWireMock;
 import org.wiremock.spring.InjectWireMock;
@@ -33,27 +27,10 @@ class SpringAddonsServerWebClientSupportTest {
     @InjectWireMock
     private WireMockServer wireMockServer;
 
-    private TestExecutionListener executionListener = new ReactorContextTestExecutionListener();
-
-    private final String token = "TOKEN";
-
-    @BeforeEach
-    void setUp() throws Exception {
-        var authentication = new JwtAuthenticationToken(Jwt.withTokenValue(token)
-                .header("HEADERS", "CANNOT_BE_EMPTY")
-                .claim("CLAIMS", "CANNOT_BE_EMPTY")
-                .build()
-        );
-        TestSecurityContextHolder.setAuthentication(authentication);
-        executionListener.beforeTestMethod(null);
-    }
-
-    @AfterEach
-    public void cleanup() throws Exception {
-        executionListener.afterTestMethod(null);
-    }
+    private final String token = "machin.truc.bidule";
 
     @Test
+    @WithJwt(file = "ch4mp.json", bearerString = token)
     void givenUserIsAuthenticated_whenSendRequestWithForwardingBearerExchangeFilterFunction_thenAuthorizationHeaderIsSet() {
         // given
         var url = "/forward/bearer";
