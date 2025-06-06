360bf43f9eee29b5d15e71e9c7a1640e8b50f1bf
ch4mpy <ch4mp@c4-soft.com>
2025-06-05 14:42:03 -1000

    Bump Boot to 3.5.0

--- samples/tutorials/resource-server_with_ui/src/test/java/com/c4soft/springaddons/tutorials/ResourceServerWithUiApplicationTests.java
+++ samples/tutorials/resource-server_with_ui/src/test/java/com/c4soft/springaddons/tutorials/ResourceServerWithUiApplicationTests.java
@@ -2,15 +2,14 @@ package com.c4soft.springaddons.tutorials;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.security.test.context.support.WithAnonymousUser;
-
 import com.c4_soft.springaddons.security.oauth2.test.annotations.WithJwt;
 import com.c4_soft.springaddons.security.oauth2.test.webmvc.AddonsWebmvcTestConf;
 import com.c4_soft.springaddons.security.oauth2.test.webmvc.AutoConfigureAddonsWebmvcResourceServerSecurity;
@@ -19,22 +18,21 @@ import com.c4_soft.springaddons.security.oauth2.test.webmvc.MockMvcSupport;
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK)
 @AutoConfigureMockMvc
 @AutoConfigureAddonsWebmvcResourceServerSecurity
-@ImportAutoConfiguration({ AddonsWebmvcTestConf.class })
+@ImportAutoConfiguration({AddonsWebmvcTestConf.class, OAuth2ClientProperties.class})
 class ResourceServerWithUiApplicationTests {
-	@Autowired
-	MockMvcSupport api;
+  @Autowired
+  MockMvcSupport api;
 
-	@Test
-	@WithAnonymousUser
-	void givenRequestIsAnonymous_whenApiGreet_thenUnauthorized() throws Exception {
-		api.get("/api/greet").andExpect(status().isUnauthorized());
-	}
+  @Test
+  @WithAnonymousUser
+  void givenRequestIsAnonymous_whenApiGreet_thenUnauthorized() throws Exception {
+    api.get("/api/greet").andExpect(status().isUnauthorized());
+  }
 
-	@Test
-	@WithJwt("ch4mp_keycloak.json")
-	void givenUserIsAuthenticated_whenApiGreet_thenOk() throws Exception {
-		api.get("/api/greet").andExpect(status().isOk()).andExpect(
-				content().string(
-						"Hi 4dd56dbb-71ef-4fe2-9358-3ae3240a9e94! You are authenticated by http://localhost:7080/auth/realms/spring-addons and granted with: [NICE, AUTHOR]."));
-	}
+  @Test
+  @WithJwt("ch4mp_keycloak.json")
+  void givenUserIsAuthenticated_whenApiGreet_thenOk() throws Exception {
+    api.get("/api/greet").andExpect(status().isOk()).andExpect(content().string(
+        "Hi 4dd56dbb-71ef-4fe2-9358-3ae3240a9e94! You are authenticated by http://localhost:7080/auth/realms/spring-addons and granted with: [NICE, AUTHOR]."));
+  }
 }
