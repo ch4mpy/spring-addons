7420d2453a25283e3aed052f6d165536ba1ededa
Jérôme Wacongne <ch4mp@c4-soft.com>
2025-06-15 08:12:07 -1000

    Fix roles with spaces in string list claims (#276)

--- spring-addons-starter-oidc/src/main/java/com/c4_soft/springaddons/security/oidc/starter/ConfigurableClaimSetAuthoritiesConverter.java
+++ spring-addons-starter-oidc/src/main/java/com/c4_soft/springaddons/security/oidc/starter/ConfigurableClaimSetAuthoritiesConverter.java
@@ -4,17 +4,14 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
-
 import org.springframework.lang.NonNull;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.util.StringUtils;
-
 import com.c4_soft.springaddons.security.oidc.starter.properties.NotAConfiguredOpenidProviderException;
 import com.c4_soft.springaddons.security.oidc.starter.properties.SpringAddonsOidcProperties.OpenidProviderProperties.SimpleAuthoritiesMappingProperties;
 import com.jayway.jsonpath.JsonPath;
 import com.jayway.jsonpath.PathNotFoundException;
-
 import lombok.RequiredArgsConstructor;
 
 /**
@@ -22,77 +19,80 @@ import lombok.RequiredArgsConstructor;
  * Portable converter to extract Spring-security authorities from OAuth2 claims.
  * </p>
  * <p>
- * It relies on {@link OpenidProviderPropertiesResolver} to resolve the configuration properties for the provided claims (and throws if it
- * is not resolved). This properties enable to configure:
+ * It relies on {@link OpenidProviderPropertiesResolver} to resolve the configuration properties for
+ * the provided claims (and throws if it is not resolved). This properties enable to configure:
  * </p>
  * <ul>
  * <li>source claims (which claims to pick authorities from, dot.separated.path is supported)</li>
- * <li>for each claim: prefix (should anything like "ROLE_" or "PREFIX_" be pre-pended to authorities)</li>
- * <li>for each claim: case transformation (should be authorities be forced to uppercase, lowercase or be left untouched)</li>
+ * <li>for each claim: prefix (should anything like "ROLE_" or "PREFIX_" be pre-pended to
+ * authorities)</li>
+ * <li>for each claim: case transformation (should be authorities be forced to uppercase, lowercase
+ * or be left untouched)</li>
  * </ul>
  *
  * @author ch4mp
  */
 @RequiredArgsConstructor
 public class ConfigurableClaimSetAuthoritiesConverter implements ClaimSetAuthoritiesConverter {
-	private final OpenidProviderPropertiesResolver opPropertiesResolver;
+  private final OpenidProviderPropertiesResolver opPropertiesResolver;
 
-	@Override
-	public Collection<? extends GrantedAuthority> convert(@NonNull Map<String, Object> source) {
-		final var opProperties = opPropertiesResolver.resolve(source).orElseThrow(() -> new NotAConfiguredOpenidProviderException(source));
-		// @formatter:off
+  @Override
+  public Collection<? extends GrantedAuthority> convert(@NonNull Map<String, Object> source) {
+    final var opProperties = opPropertiesResolver.resolve(source)
+        .orElseThrow(() -> new NotAConfiguredOpenidProviderException(source));
+    // @formatter:off
 	    return opProperties.getAuthorities().stream()
 	            .flatMap(authoritiesMappingProps -> getAuthorities(source, authoritiesMappingProps))
 	            .map(r -> (GrantedAuthority) new SimpleGrantedAuthority(r)).toList();
 	    // @formatter:on
-	}
+  }
 
-	private static String processCase(String role, SimpleAuthoritiesMappingProperties.Case caze) {
-		switch (caze) {
-		case UPPER: {
-			return role.toUpperCase();
-		}
-		case LOWER: {
-			return role.toLowerCase();
-		}
-		default:
-			return role;
-		}
-	}
+  private static String processCase(String role, SimpleAuthoritiesMappingProperties.Case caze) {
+    switch (caze) {
+      case UPPER: {
+        return role.toUpperCase();
+      }
+      case LOWER: {
+        return role.toLowerCase();
+      }
+      default:
+        return role;
+    }
+  }
 
-	private static Stream<String> getAuthorities(Map<String, Object> claims, SimpleAuthoritiesMappingProperties props) {
-		// @formatter:off
+  private static Stream<String> getAuthorities(Map<String, Object> claims,
+      SimpleAuthoritiesMappingProperties props) {
+    // @formatter:off
 	    return getClaims(claims, props.getPath())
-	    		.flatMap(claim -> Stream.of(claim.split(",")))
-	    		.flatMap(claim -> Stream.of(claim.split(" ")))
 	    		.filter(StringUtils::hasText)
 	    		.map(String::trim)
 	            .map(r -> processCase(r, props.getCaze()))
 	            .map(r -> String.format("%s%s", props.getPrefix(), r));
 	    // @formatter:on
-	}
+  }
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	private static Stream<String> getClaims(Map<String, Object> claims, String path) {
-		try {
-			final var res = JsonPath.read(claims, path);
-			if (res instanceof String r) {
-				return Stream.of(r);
-			}
-			if (res instanceof List l) {
-				if (l.size() == 0) {
-					return Stream.empty();
-				}
-				if (l.get(0) instanceof String) {
-					return l.stream();
-				}
-				if (l.get(0) instanceof List) {
-					return l.stream().flatMap(o -> ((List) o).stream());
-				}
-			}
-			return Stream.empty();
-		} catch (PathNotFoundException e) {
-			return Stream.empty();
-		}
-	}
+  @SuppressWarnings({"rawtypes", "unchecked"})
+  private static Stream<String> getClaims(Map<String, Object> claims, String path) {
+    try {
+      final var res = JsonPath.read(claims, path);
+      if (res instanceof String r) {
+        return Stream.of(r).flatMap(claim -> Stream.of(claim.split(",")))
+            .flatMap(claim -> Stream.of(claim.split(" ")));
+      }
+      if (res instanceof List l) {
+        if (l.size() == 0) {
+          return Stream.empty();
+        }
+        if (l.get(0) instanceof String) {
+          return l.stream();
+        }
+        if (l.get(0) instanceof List) {
+          return l.stream().flatMap(o -> ((List) o).stream());
+        }
+      }
+      return Stream.empty();
+    } catch (PathNotFoundException e) {
+      return Stream.empty();
+    }
+  }
 }
--- spring-addons-starter-oidc/src/test/java/com/c4_soft/springaddons/security/oidc/starter/ConfigurableJwtGrantedAuthoritiesConverterTest.java
+++ spring-addons-starter-oidc/src/test/java/com/c4_soft/springaddons/security/oidc/starter/ConfigurableJwtGrantedAuthoritiesConverterTest.java
@@ -1,19 +1,16 @@
 package com.c4_soft.springaddons.security.oidc.starter;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.time.Instant;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtClaimNames;
-
 import com.c4_soft.springaddons.security.oidc.OpenidClaimSet;
 import com.c4_soft.springaddons.security.oidc.starter.properties.SpringAddonsOidcProperties;
 import com.c4_soft.springaddons.security.oidc.starter.properties.SpringAddonsOidcProperties.OpenidProviderProperties;
@@ -22,60 +19,68 @@ import com.c4_soft.springaddons.security.oidc.starter.properties.SpringAddonsOid
 
 public class ConfigurableJwtGrantedAuthoritiesConverterTest {
 
-	@Test
-	public void test() throws URISyntaxException {
-		final var issuer = new URI("https://authorisation-server");
+  @Test
+  public void test() throws URISyntaxException {
+    final var issuer = new URI("https://authorisation-server");
 
-		final var client1Roles = List.of("R11", "r12");
+    final var client1Roles = List.of("R11", "r12");
 
-		final var client2Roles = List.of("R21", "r22");
+    final var client2Roles = List.of("R21", "r22");
 
-		final var client3Roles = List.of("R31", "r32");
+    final var client3Roles = List.of("R31", "r32");
 
-		final var realmRoles = List.of("r1", "r2");
+    final var realmRoles = List.of("r1", "r 2");
 
-		// @formatter:off
+    final var scope = "s1 s2";
+
+    // @formatter:off
 		final var claims = Map.of(
 				JwtClaimNames.ISS, issuer,
 				"resource_access", Map.of(
 						"client1", Map.of("roles", client1Roles),
 						"client2", Map.of("roles", client2Roles.stream().collect(Collectors.joining(", "))),
 						"client3", Map.of("roles", client3Roles.stream().collect(Collectors.joining(" ")))),
-				"realm_access", Map.of("roles", realmRoles));
+				"realm_access", Map.of("roles", realmRoles),
+				"scope", scope);
 		// @formatter:on
 
-		final var now = Instant.now();
-		final var jwt = new Jwt("a.b.C", now, Instant.ofEpochSecond(now.getEpochSecond() + 3600), Map.of("machin", "truc"), claims);
+    final var now = Instant.now();
+    final var jwt = new Jwt("a.b.C", now, Instant.ofEpochSecond(now.getEpochSecond() + 3600),
+        Map.of("machin", "truc"), claims);
 
-		final var issuerProperties = new OpenidProviderProperties();
-		issuerProperties.setIss(issuer);
+    final var issuerProperties = new OpenidProviderProperties();
+    issuerProperties.setIss(issuer);
 
-		final var properties = new SpringAddonsOidcProperties();
-		properties.setOps(List.of(issuerProperties));
+    final var properties = new SpringAddonsOidcProperties();
+    properties.setOps(List.of(issuerProperties));
 
-		final var converter = new ConfigurableClaimSetAuthoritiesConverter(new ByIssuerOpenidProviderPropertiesResolver(properties));
-		final var claimSet = new OpenidClaimSet(jwt.getClaims());
+    final var converter = new ConfigurableClaimSetAuthoritiesConverter(
+        new ByIssuerOpenidProviderPropertiesResolver(properties));
+    final var claimSet = new OpenidClaimSet(jwt.getClaims());
 
-		// Assert mapping with default properties
-		assertThat(converter.convert(claimSet).stream().map(GrantedAuthority::getAuthority).toList()).isEmpty();
+    // Assert mapping with default properties
+    assertThat(converter.convert(claimSet).stream().map(GrantedAuthority::getAuthority).toList())
+        .isEmpty();
 
-		// Assert with prefix & uppercase
-		issuerProperties.setAuthorities(
-				List.of(
-						simpleAuthoritiesMappingProperties("$.realm_access.roles", "MACHIN_", Case.UNCHANGED),
-						simpleAuthoritiesMappingProperties("resource_access.client1.roles", "TRUC_", Case.LOWER),
-						simpleAuthoritiesMappingProperties("resource_access.client3.roles", "CHOSE_", Case.UPPER)));
+    // Assert with prefix & uppercase
+    issuerProperties.setAuthorities(List.of(
+        simpleAuthoritiesMappingProperties("$.realm_access.roles", "MACHIN_", Case.UNCHANGED),
+        simpleAuthoritiesMappingProperties("resource_access.client1.roles", "TRUC_", Case.LOWER),
+        simpleAuthoritiesMappingProperties("resource_access.client3.roles", "CHOSE_", Case.UPPER),
+        simpleAuthoritiesMappingProperties("scope", "SCOPE_", Case.UNCHANGED)));
 
-		assertThat(converter.convert(claimSet).stream().map(GrantedAuthority::getAuthority).toList())
-				.containsExactlyInAnyOrder("TRUC_r11", "TRUC_r12", "CHOSE_R31", "CHOSE_R32", "MACHIN_r1", "MACHIN_r2");
-	}
+    assertThat(converter.convert(claimSet).stream().map(GrantedAuthority::getAuthority).toList())
+        .containsExactlyInAnyOrder("TRUC_r11", "TRUC_r12", "CHOSE_R31", "CHOSE_R32", "MACHIN_r1",
+            "MACHIN_r 2", "SCOPE_s1", "SCOPE_s2");
+  }
 
-	private static SimpleAuthoritiesMappingProperties simpleAuthoritiesMappingProperties(String jsonPath, String prefix, Case caseTransformation) {
-		final var props = new SimpleAuthoritiesMappingProperties();
-		props.setCaze(caseTransformation);
-		props.setPath(jsonPath);
-		props.setPrefix(prefix);
-		return props;
-	}
+  private static SimpleAuthoritiesMappingProperties simpleAuthoritiesMappingProperties(
+      String jsonPath, String prefix, Case caseTransformation) {
+    final var props = new SimpleAuthoritiesMappingProperties();
+    props.setCaze(caseTransformation);
+    props.setPath(jsonPath);
+    props.setPrefix(prefix);
+    return props;
+  }
 
 }
