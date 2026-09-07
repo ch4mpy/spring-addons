060112ce72c8d0d5edf16f58b8a231ea2cf9eb56
Jerome WACONGNE <j.wacongne@tahitinumerique.pf>
2026-09-07 11:38:52 -1000

    Create release script

---
+++ release.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sdk env
+export JDK_JAVA_OPTIONS='--add-opens java.base/java.util=ALL-UNNAMED --add-opens java.base/java.lang.reflect=ALL-UNNAMED --add-opens java.base/java.text=ALL-UNNAMED --add-opens java.desktop/java.awt.font=ALL-UNNAMED'
+mvn clean install release:prepare release:perform
